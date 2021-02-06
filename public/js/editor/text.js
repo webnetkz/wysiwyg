3df65f541d1122ce934f7dd69bec8a68147cecae
webnetkz <dbqqbq@gmail.com>
2021-02-07 00:55:40 +0600

    update

--- public/js/editor/text.js
+++ public/js/editor/text.js
@@ -4,7 +4,7 @@ window.addEventListener('click', () => {
     if(focusElem.anchorNode && focusElem.anchorNode.nodeName == "#text") {
         headerNav.innerHTML = '<div class="notEdit allTopNav">'+
             '<div class="notEdit">'+
-            	'<p>Цвет текта</p>'+
+            	'<span class="notEdit elementTopNav">Цвет текста</span>'+
                 '<div class="colorPicker">'+
                     '<div onclick="changeColorText(\'white\');" style="background: white"></div>'+
                     '<div onclick="changeColorText(\'red\');" style="background: red"></div>'+
@@ -18,6 +18,9 @@ window.addEventListener('click', () => {
                     '<div onclick="changeColorText(\'orange\');" style="background: orange"></div>'+
                 '</div>'+
             '</div>'+
+             '<div class="notEdit">'+
+            	'<span onclick="appendLinkText();" class="notEdit elementTopNav">Ссылка</span>'+
+            '</div>'+
         '</div>';
     }
 
@@ -30,5 +33,16 @@ function changeColorText(color) {
     newNode.style.color = color;
 
     range.surroundContents(newNode);
+    return false;
+}
+
+function appendLinkText() {
+    let range = window.getSelection().getRangeAt(0);
+    let newNode = document.createElement("a");
+  	let linkAnswer = prompt('Введите ссылку');
+  	newNode.href = linkAnswer;
+
+    range.surroundContents(newNode);
+    document.getSelection().removeAllRanges();
     return false;
 }
